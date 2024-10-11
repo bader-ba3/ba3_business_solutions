58b03722d4605f794a0f71a95b4390a44c3fbada
Ahmed Zein <ahmedzezo1080@gmail.com>
2024-10-11 17:47:28 +0400

    Merge remote-tracking branch 'origin/main'

--- lib/core/shared/widgets/custom_pluto_with_edite.dart
+++ lib/core/shared/widgets/custom_pluto_with_edite.dart
@@ -3,15 +3,14 @@ import 'package:pluto_grid/pluto_grid.dart';
 
 import 'custom_pluto_grid_style_config.dart';
 
-
 class CustomPlutoWithEdite extends StatelessWidget {
   const CustomPlutoWithEdite({
     super.key,
     required this.controller,
-     this.shortCut,
+    this.shortCut,
     this.onChanged,
     required this.onRowSecondaryTap,
-    this.evenRowColor=Colors.blueAccent,
+    this.evenRowColor = Colors.blueAccent,
   });
 
   final dynamic controller;
@@ -25,14 +24,13 @@ class CustomPlutoWithEdite extends StatelessWidget {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: PlutoGrid(
-
         columns: controller.columns,
         rows: controller.rows,
         onRowSecondaryTap: onRowSecondaryTap,
         onChanged: onChanged,
         configuration: PlutoGridConfiguration(
-          shortcut: shortCut??const PlutoGridShortcut(),
-          style: buildGridStyleConfig(evenRowColor:evenRowColor),
+          shortcut: shortCut ?? const PlutoGridShortcut(),
+          style: buildGridStyleConfig(evenRowColor: evenRowColor),
           localeText: const PlutoGridLocaleText.arabic(),
         ),
         onLoaded: (PlutoGridOnLoadedEvent event) {
@@ -40,12 +38,15 @@ class CustomPlutoWithEdite extends StatelessWidget {
           final newRows = controller.stateManager.getNewRows(count: 30);
           controller.stateManager.appendRows(newRows);
 
-          if (controller.stateManager.rows.isNotEmpty && controller.stateManager.rows.first.cells.length > 1) {
-            final secondCell = controller.stateManager.rows.first.cells.entries.elementAt(1).value;
+          if (controller.stateManager.rows.isNotEmpty &&
+              controller.stateManager.rows.first.cells.length > 1) {
+            final secondCell = controller.stateManager.rows.first.cells.entries
+                .elementAt(1)
+                .value;
             controller.stateManager.setCurrentCell(secondCell, 0);
 
-            FocusScope.of(event.stateManager.gridFocusNode.context!).requestFocus(event.stateManager.gridFocusNode);
-
+            FocusScope.of(event.stateManager.gridFocusNode.context!)
+                .requestFocus(event.stateManager.gridFocusNode);
           }
         },
       ),
